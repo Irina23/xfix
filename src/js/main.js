ecdc11e1a63b4526f18c254c2d5f99d48914cd2d
admin <ira.intertech@gmail.com>
2016-11-17 15:52:48 +0200

    block form master, form calculation

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,8 @@ if ($.browser.mobile) $('body').addClass('mobile');
 if ($.browser.safari) $('body').addClass('safari');
 if ($.browser.iphone || $.browser.ipad || $.browser.ipod ) $('body').addClass('ios');
 
-$(document).on('ready', function () {
-	var $window = $(window),
-		windowWidth = $window.width(),
-		windowHeight = $window.height(),
-		bodyHeight = $('body').height();
-
-
-		// resize
-		$window.on('resize', function () {
-
-			windowWidth = $window.width();
-			windowHeight = $window.height();
-			bodyHeight = $('body').height();
-
-		});
+$(document).ready( function () {
+	$("form").validate();
+	$('select').selectbox();
 
 });
